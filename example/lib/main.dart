16ea59488f5730ede057e0b91ea0e808a3e654c1
Fitz Leopold <stendan7@163.com>
2024-10-08 15:58:15 +0800

    升级Kotlin版本并优化示例代码
    - 将Kotlin版本从1.6.10升级到1.7.10- 重构示例代码，优化UI布局和逻辑结构
    - 添加后台升级功能和相关配置
    - 更新依赖库版本，确保兼容性和性能

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile_upgrader/flutter_mobile_upgrader.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -14,6 +15,9 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
+  final String apkDownloadUrl = '';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,48 +27,107 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Padding(
           padding: const EdgeInsets.only(top: 28.0),
-          child: Row(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Expanded(
-                child: ElevatedButton(
-                  style: ElevatedButtonTheme.of(context).style,
-                  child: const Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text(
-                      '升 级',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  onPressed: () {
-                    final Future<AppUpgradeInfo> appUpgradeInfo = Future.value(
-                      AppUpgradeInfo(
-                          title: '更新提示',
-                          contents: ['有新版本哟,请更新～'],
-                          force: false,
-                          apkDownloadUrl: ''),
-                    );
-
-                    if (Platform.isAndroid) {
-                      AppUpgradeManager.upgrade(
-                        context,
-                        appUpgradeInfo,
-                      );
-                    }
-
-                    if (Platform.isIOS) {
-                      AppUpgradeManager.upgrade(
-                        context,
-                        appUpgradeInfo,
-                        iosAppId: 'idxxxxxx',
-                      );
-                    }
-                  },
-                ),
-              ),
+              _buildNormal(context),
+              const SizedBox(height: 20),
+              _buildBackMode(context),
             ],
-          ),
+          )
         ),
       ),
+    );
+  }
+
+  Widget _buildNormal (BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          style: ElevatedButtonTheme.of(context).style,
+          child: const Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Text(
+              '普通升级',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          onPressed: () {
+            final Future<AppUpgradeInfo> appUpgradeInfo = Future.value(
+              AppUpgradeInfo(
+                  title: '更新提示',
+                  contents: ['有新版本哟,请更新～'],
+                  force: false,
+                  apkDownloadUrl: apkDownloadUrl),
+            );
+
+            if (Platform.isAndroid) {
+              AppUpgradeManager.upgrade(
+                context,
+                appUpgradeInfo,
+              );
+            }
+
+            if (Platform.isIOS) {
+              AppUpgradeManager.upgrade(
+                context,
+                appUpgradeInfo,
+                iosAppId: 'idxxxxxx',
+              );
+            }
+          },
+        )
+      ],
+    );
+  }
+
+  Widget _buildBackMode (BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        ElevatedButton(
+          style: ElevatedButtonTheme.of(context).style,
+          child: const Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Text(
+              '后台升级',
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+          onPressed: () {
+            final Future<AppUpgradeInfo> appUpgradeInfo = Future.value(
+              AppUpgradeInfo(
+                  title: '更新提示',
+                  contents: ['有新版本哟,请更新～'],
+                  force: false,
+                  isBackground: true,
+                  apkDownloadUrl: apkDownloadUrl),
+            );
+
+            if (Platform.isAndroid) {
+              AppUpgradeManager.upgrade(
+                context,
+                appUpgradeInfo,
+                okText: '后台更新',
+                onOk: () async {
+                  debugPrint('开始后台更新');
+                }
+              );
+            }
+
+            if (Platform.isIOS) {
+              AppUpgradeManager.upgrade(
+                context,
+                appUpgradeInfo,
+                iosAppId: 'idxxxxxx',
+              );
+            }
+          },
+        )
+      ],
     );
   }
 }
